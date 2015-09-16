de0083b1b4a9882d4937d3faf1900ddfcde90243
kiwenlau <kiwenlau@126.com>
2015-09-16 15:49:20 +0900

    kiwenlau/single-mesos:2.0

--- Dockerfile
+++ Dockerfile
@@ -10,12 +10,12 @@ echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc
 sudo apt-get -y update
 
 # Install mesos and marathon (The Mesos package will automatically pull in the ZooKeeper package as a dependency)
-RUN sudo apt-get -y install mesos marathon
+RUN sudo apt-get -y install vim mesos marathon
 
 EXPOSE 8080 5050
 
 # init script
 ADD start-cluster.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/start-cluster.sh
-#CMD ["/usr/local/bin/start-cluster.sh"]
+CMD '/usr/local/bin/start-cluster.sh'; bash
 
--- start-cluster.sh
+++ start-cluster.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 # Start ZooKeeper
-sudo service zookeeper restart
+echo "Start ZooKeeper..."
+sudo service zookeeper restart 
 
-# Start Mesos and Marathon
-sudo service mesos-master restart
-sudo service mesos-slave restart
-sudo service marathon restart
+# Start Mesos master
+echo "Start Mesos master..."
+exec /usr/sbin/mesos-master --zk=zk://127.0.0.1:2181/mesos --quorum=1 --work_dir=/var/lib/mesos --log_dir=/log/mesos  >>/dev/null 2>&1 &
+
+# Start Mesos slave
+echo "Start Mesos slave..."
+exec /usr/sbin/mesos-slave --master=zk://127.0.0.1:2181/mesos --log_dir=/log/mesos >>/dev/null 2>&1 &
+
+# Start Marathon
+echo "Start Marathon..."
+exec sudo service marathon start 
